3809e210d38601f306a02f42e1c5c977268b0188
David Mórász <m@mcro.de>
2023-12-09 12:28:14 +0100

    configurable logo rendering

--- components/App.jsx
+++ components/App.jsx
@@ -5,6 +5,7 @@ import Parameters from 'echweb-content/js/Parameters';
 import Logo from 'echweb-content/js/Logo';
 import SvgFilters from 'echweb-shared/SvgFilters';
 import MainScrollbar from './MainScrollbar';
+import {Gh1, Gh2} from 'echweb-shared/Gh';
 
 export default class App extends React.Component {
     constructor(props) {
@@ -13,21 +14,32 @@ export default class App extends React.Component {
     
     render() {
         const { children, intro } = this.props;
+        let hprops = {
+            className: "h0",
+            id: "mainHead",
+            style: {
+                marginTop: "0px",
+                marginBottom: "-10px"
+            }
+        }
+
+        let SelectedHeader = props => <h1 {...hprops}>{props.children}</h1>;
+        if ('glitch' in Parameters.header) {
+            SelectedHeader = props => <Gh1 glitchtype="1" {...hprops}>{props.children}</Gh1>;
+        }
+
+        let allowHeader = ('allowHeaderOnIntro' in Parameters.header) || !intro;
 
         return <>
             <SvgFilters />
             <div id="appRoot">
                 {intro ? <Logo /> : <div style={{display: "none"}} />}
-                {intro ? <></> : <h1
-                    className="h0"
-                    id="mainHead"
-                    style={{
-                        marginTop: "0px",
-                        marginBottom: "-10px"
-                    }}
-                >
-                    <Link to="/">{Parameters.header.name}</Link>
-                </h1>}
+                {allowHeader
+                    ? <SelectedHeader>
+                        <Link to="/">{Parameters.header.name}</Link>
+                    </SelectedHeader>
+                    : <></>
+                }
                 <MainMenu />
                 {children}
                 <div id="footer"></div>
