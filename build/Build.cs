80fd18091913e539adce23794a5db9a96254e870
David Mórász <m@mcro.de>
2023-12-27 13:49:22 +0100

    deploy all from local machine

--- build/Build.cs
+++ build/Build.cs
@@ -3,7 +3,9 @@ using System.Linq;
 using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tooling;
+using Nuke.Common.Tools.Git;
 using Nuke.Common.Utilities;
+using Serilog;
 
 class BuildMain : NukeBuild
 {
@@ -12,48 +14,122 @@ class BuildMain : NukeBuild
     [PathVariable("npm")]
     Tool NPM;
 
+    void InstallSharedBody() => NPM("install", logInvocation: true, workingDirectory: RootDirectory / "shared");
     Target InstallShared => _ => _
-        .Executes(() => NPM("install", workingDirectory: RootDirectory / "shared"));
+        .Executes(InstallSharedBody);
 
+    void InstallContentBody() => NPM("install", logInvocation: true, workingDirectory: RootDirectory / "content");
     Target InstallContent => _ => _
         .DependsOn(InstallShared)
-        .Executes(() => NPM("install", workingDirectory: RootDirectory / "content"));
+        .Executes(InstallContentBody);
 
+    void InstallBody() => NPM("install", logInvocation: true, workingDirectory: RootDirectory);
     Target Install => _ => _
         .DependsOn(InstallContent)
-        .Executes(() => NPM("install", workingDirectory: RootDirectory));
+        .Executes(InstallBody);
 
+    void PrepareBody() => NPM("run mdPrepare", logInvocation: true, workingDirectory: RootDirectory);
     Target Prepare => _ => _
         .After(Install)
-        .Executes(() =>
-        {
-            NPM("run mdPrepare", workingDirectory: RootDirectory);
-        });
+        .Executes(PrepareBody);
 
+    void BuildBody()
+    {
+        NPM("run build", workingDirectory: RootDirectory);
+        FileSystemTasks.CopyDirectoryRecursively(
+            RootDirectory / "content",
+            RootDirectory / "dist" / "content",
+            DirectoryExistsPolicy.Merge,
+            FileExistsPolicy.Overwrite,
+            d => d.Name.EqualsAnyOrdinalIgnoreCase(
+                "node_modules",
+                "js",
+                ".git",
+                ".github",
+                "dist"
+            ),
+            f => f.Name.EqualsOrdinalIgnoreCase("package.json")
+        );
+        FileSystemTasks.CopyDirectoryRecursively(
+            RootDirectory / "content" / "dist",
+            RootDirectory / "dist",
+            DirectoryExistsPolicy.Merge,
+            FileExistsPolicy.Overwrite
+        );
+    }
     Target Build => _ => _
         .After(Install, Prepare)
+        .Executes(BuildBody);
+
+    record ContentDestinationPair(string Content, string Destination);
+
+    Target Deploy => _ => _
         .Executes(() =>
         {
-            NPM("run build", workingDirectory: RootDirectory);
-            FileSystemTasks.CopyDirectoryRecursively(
-                RootDirectory / "content",
-                RootDirectory / "dist" / "content",
-                DirectoryExistsPolicy.Merge,
-                FileExistsPolicy.Overwrite,
-                d => d.Name.EqualsAnyOrdinalIgnoreCase(
-                    "node_modules",
-                    "js",
-                    ".git",
-                    ".github",
-                    "dist"
-                ),
-                f => f.Name.EqualsOrdinalIgnoreCase("package.json")
-            );
-            FileSystemTasks.CopyDirectoryRecursively(
-                RootDirectory / "content" / "dist",
-                RootDirectory / "dist",
-                DirectoryExistsPolicy.Merge,
-                FileExistsPolicy.Overwrite
-            );
+            var remotes = new ContentDestinationPair[] {
+                new("https://github.com/microdee/echweb-mcrode.git", "https://github.com/microdee/microdee.github.io.git"),
+                // new("https://github.com/holy-olga/holy-olga.github.io-dev.git", "https://github.com/holy-olga/holy-olga.github.io.git"),
+            };
+
+            var inputContent = RootDirectory / "Content";
+            var tempDist = RootDirectory / "dist_temp";
+            var dist = RootDirectory / "dist";
+
+            foreach(var (contentGit, dstGit) in remotes)
+            {
+                Log.Information("---- Building from content from {0} ----", contentGit);
+                if (inputContent.DirectoryExists()) inputContent.DeleteDirectory();
+                if (tempDist.DirectoryExists()) tempDist.DeleteDirectory();
+                if (dist.DirectoryExists()) dist.DeleteDirectory();
+
+                Log.Information("Cloning content");
+                GitTasks.Git(
+                    $"clone  --depth 1 --recurse-submodules {contentGit} {inputContent}",
+                    workingDirectory: RootDirectory,
+                    logInvocation: true
+                );
+                
+                Log.Information("NPM install");
+                InstallSharedBody();
+                InstallContentBody();
+                InstallBody();
+                
+                Log.Information("Preparing content");
+                PrepareBody();
+                
+                Log.Information("Building site");
+                BuildBody();
+
+                Log.Information("Cloning destination");
+                GitTasks.Git(
+                    $"clone --recurse-submodules {dstGit} {tempDist}",
+                    workingDirectory: RootDirectory,
+                    logInvocation: true
+                );
+
+                Log.Information("Exchanging .git folder");
+                FileSystemTasks.MoveDirectoryToDirectory(
+                    tempDist / ".git",
+                    dist,
+                    DirectoryExistsPolicy.Fail
+                );
+
+                Log.Information("Pushing new content");
+                GitTasks.Git(
+                    $"add .",
+                    workingDirectory: dist,
+                    logInvocation: true
+                );
+                GitTasks.Git(
+                    $"commit -m \"deploy from local\"",
+                    workingDirectory: dist,
+                    logInvocation: true
+                );
+                GitTasks.Git(
+                    $"push",
+                    workingDirectory: dist,
+                    logInvocation: true
+                );
+            }
         });
 }
